3c497aaffada7e32826b3416a18cfe47d003c6fc
berkealgul <berkealgul@gmail.com>
2023-01-04 06:13:11 +0300

    image reading done

--- src/dataset_publisher_node.cpp
+++ src/dataset_publisher_node.cpp
@@ -1,3 +1,6 @@
+#include <sstream>
+#include <iomanip>
+
 #include <chrono>
 #include <functional>
 #include <memory>
@@ -8,46 +11,70 @@
 #include "sensor_msgs/msg/image.hpp"
 #include "opencv2/core/core.hpp"
 #include <cv_bridge/cv_bridge.h>
+#include <opencv2/highgui.hpp>
 
 
 using namespace std::chrono_literals;
 
-class DatesetNode : public rclcpp::Node
+class DatasetNode : public rclcpp::Node
 {
 public:
     DatasetNode(): Node("dateset_publisher"), count_(0)
     {
-		publisher_ = this->create_publisher<sensor_msgs::msg::Image>("topic", 10);
-		timer_ = this->create_wall_timer(500ms, std::bind(&MinimalPublisher::timer_callback, this));
+		// publisher_ = this->create_publisher<sensor_msgs::msg::Image>("topic", 10);
+		// timer_ = this->create_wall_timer(500ms, std::bind(&MinimalPublisher::timer_callback, this));
+		read_dataset_once();
     }	
 
 private:
-    void timer_callback()
-    {
-		auto message = std_msgs::msg::String();
-		message.data = "Hello, world! " + std::to_string(count_++);
-		RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-		publisher_->publish(message);
 
-	  	cv_bridge::CvImagePtr cv_ptr;
-		cv_ptr = cv_bridge::toCvCopy(request->depth_image, request->depth_image.encoding);
-		cv::patchNaNs(cv_ptr->image, 0.0);
-		// Datatype depends on your camera image encoding!
-		if (cv_ptr->image.at<float>(row, col) >= 0.001)
+	int data_counter = 0;
+	std::string p = "/home/berke/Downloads/rgbd-scenes-v2_imgs/rgbd-scenes-v2/imgs/scene_01/";
+
+	void read_dataset_once()
+	{
+		while(data_counter < 50)
 		{
-			depth = cv_ptr->image.at<float>(row, col);
-			return true;
+			std::stringstream path_stream;
+			path_stream << p << std::setw(5) << std::setfill('0') << data_counter << "-depth.png";
+			std::string path = path_stream.str();
+			std::cout << path << std::endl;
+			cv::Mat mat = cv::imread(path, cv::IMREAD_ANYDEPTH);
+			cv::imshow("asd", mat);
+			cv::waitKey(20);
+			data_counter++;
 		}
-    }
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+
+
+	}
+
+
+	// void timer_callback()
+	// {
+	// 	auto message = std_msgs::msg::String();
+	// 	message.data = "Hello, world! " + std::to_string(count_++);
+	// 	RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+	// 	publisher_->publish(message);
+
+	// 	cv_bridge::CvImagePtr cv_ptr;
+	// 	cv_ptr = cv_bridge::toCvCopy(request->depth_image, request->depth_image.encoding);
+	// 	cv::patchNaNs(cv_ptr->image, 0.0);
+	// 	// Datatype depends on your camera image encoding!
+	// 	if (cv_ptr->image.at<float>(row, col) >= 0.001)
+	// 	{
+	// 		depth = cv_ptr->image.at<float>(row, col);
+	// 		return true;
+	// 	}
+	// }
+	// rclcpp::TimerBase::SharedPtr timer_;
+    // rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
     size_t count_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<DatesetNode>());
+  rclcpp::spin(std::make_shared<DatasetNode>());
   rclcpp::shutdown();
   return 0;
 }
--- src/demaloc.h
+++ src/demaloc.h
@@ -1,22 +1,65 @@
-#include "octomap/AbstractOccupancyOcTree.h"
+#include "octomap/OcTree.h"
 #include <opencv2/opencv.hpp>
-#include "geometry_msgs/Pose.h"
+#include "geometry_msgs/msg/pose.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 class Demaloc
 {
 public:
     
-
     Demaloc()
     {
 
     }
 
-    void update_map(const cv::Mat& img, const geometry_msgs& pose)
+private:
+
+    int data_counter = 0;
+	std::string p = "/home/berke/Downloads/rgbd-scenes-v2_imgs/rgbd-scenes-v2/imgs/scene_01/";
+
+	void read_dataset_once()
+	{
+		while(data_counter < 50)
+		{
+			std::stringstream path_stream;
+			path_stream << p << std::setw(5) << std::setfill('0') << data_counter << "-depth.png";
+			std::string path = path_stream.str();
+			std::cout << path << std::endl;
+			cv::Mat mat = cv::imread(path, cv::IMREAD_ANYDEPTH);
+			cv::imshow("asd", mat);
+			cv::waitKey(20);
+			data_counter++;
+		}
+
+
+	}
+
+    double rawDepthToMeters(int depthValue) 
     {
-        
-        //ocmap.castRay();
+        if (depthValue < 2047) 
+        {
+            return (1.0 / ((double)(depthValue) * -0.0030711016 + 3.3309495161));
+        }
+        return 0.0f;
+    }
+
+    void update_map(cv::Mat& img, geometry_msgs::msg::Pose& pose)
+    {
+        for(int i = 0; i < img.rows; i+=1)
+        {
+            for(int j = 0; i < img.cols; i+=1)
+            {
+                cv::Point minLoc, maxLoc;
+                double min, raw;
+                cv::minMaxLoc(img, min, raw, minLoc, maxLoc);
+                double depth = rawDepthToMeters(raw);
+            }
+        }
+
+
+        octomap::point3d origin(pose.position.x, pose.position.y, pose.position.z);
+        octomap::point3d target(0, 0, 0);
+        ocmap.insertRay(origin, target);
     }
 
     void publish_map()
@@ -24,8 +67,5 @@ public:
 
     }
 
-private:
-    octomap::Octree ocmap;
-
-
+    octomap::OcTree ocmap;
 };
