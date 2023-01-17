a1c219d91595a363572daa6bf5cf4016b867874b
berkealgul <berkealgul@gmail.com>
2023-01-17 08:55:37 +0300

    launch file mostly done

--- launch/octomap_demap_launch.py
+++ launch/octomap_demap_launch.py
@@ -9,27 +9,26 @@ from launch.actions import DeclareLaunchArgument
 def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument('input_image_topic', default_value='depth/rect'),
-        DeclareLaunchArgument('resolution', default_value='0.15'),
+        DeclareLaunchArgument('output_map_topic', default_value='octomap_fullmap'),
         DeclareLaunchArgument('frame_id', default_value='map'),
         DeclareLaunchArgument('base_frame_id', default_value='base_footprint'),
-        DeclareLaunchArgument('sensor_model/max_range', default_value='-1.0'),
-        DeclareLaunchArgument('sensor_model/hit', default_value='0.7'),
-        DeclareLaunchArgument('sensor_model/miss', default_value='0.4'),
-        DeclareLaunchArgument('sensor_model/min', default_value='0.12'),
-        DeclareLaunchArgument('sensor_model/max', default_value='0.97'),
+        DeclareLaunchArgument('resolution', default_value='0.15'),
+        DeclareLaunchArgument('camera_model/fx', default_value='0.7'),
+        DeclareLaunchArgument('camera_model/fy', default_value='0.4'),
+        DeclareLaunchArgument('camera_model/cx', default_value='0.12'),
+        DeclareLaunchArgument('camera_model/cy', default_value='0.97'),
         Node(
-            package='octomap_depth_mağğing',
-            executable='demaloc',
+            package='octomap_depth_mapping',
+            executable='octomap_demap',
             output='screen',
-            remappings=[('cloud_in', LaunchConfiguration('input_cloud_topic'))],
-            parameters=[{'resolution': LaunchConfiguration('resolution'),
+            remappings=[('image_in', LaunchConfiguration('input_image_topic')),
+                        ('map_out', LaunchConfiguration('output_map_topic'))],
+            parameters=[{'resoluti1on': LaunchConfiguration('resolution'),
                          'frame_id': LaunchConfiguration('frame_id'),
                          'base_frame_id': LaunchConfiguration('base_frame_id'),
-                         'compress_map': LaunchConfiguration('compress_map'),
-                         'sensor_model/max_range': LaunchConfiguration('sensor_model/max_range'),
-                         'sensor_model/hit': LaunchConfiguration('sensor_model/hit'),
-                         'sensor_model/miss': LaunchConfiguration('sensor_model/miss'),
-                         'sensor_model/min': LaunchConfiguration('sensor_model/min'),
-                         'sensor_model/max': LaunchConfiguration('sensor_model/max')}]
+                         'camera_model/fx': LaunchConfiguration('camera_model/fx'),
+                         'camera_model/fy': LaunchConfiguration('camera_model/fy'),
+                         'camera_model/cx': LaunchConfiguration('camera_model/cx'),
+                         'camera_model/cy': LaunchConfiguration('camera_model/cy')}]
         )
     ])
