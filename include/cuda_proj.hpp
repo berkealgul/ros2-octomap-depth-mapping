4be2f8c26e920b4a6cd4b3974690c2cfe00a0063
berkealgul <berkealgul@gmail.com>
2023-01-23 21:44:50 +0300

    reformatting

--- include/cuda_proj.hpp
+++ include/cuda_proj.hpp
@@ -8,12 +8,12 @@
 namespace octomap_depth_mapping
 {
 
-__global__ void project_kernel(ushort* depth, double* pc, int width, int padding,
-    double fx, double fy, double cx, double cy,
-    double r1, double r2, double r3,
-    double r4, double r5, double r6,
-    double r7, double r8, double r9,
-    double t1, double t2, double t3);
+__global__ void project_kernel(ushort*, double*, int, int,
+    double, double, double, double,
+    double, double, double,
+    double, double, double,
+    double, double, double,
+    double, double, double);
 
 } // octomap_depth_mapping
 
