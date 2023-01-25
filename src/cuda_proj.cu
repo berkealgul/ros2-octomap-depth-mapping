6a20b409e1d8bb3da76fba6520f8acc7f04a5285
berkealgul <berkealgul@gmail.com>
2023-01-25 19:38:36 +0300

    cleanup

--- src/cuda_proj.cu
+++ src/cuda_proj.cu
@@ -1,32 +1,23 @@
 #include "cuda_proj.hpp" 
 #include "depth_conversions.hpp"
 
-
 namespace octomap_depth_mapping
 {
 
 void project_depth_img(ushort* depth, double* pc, int width, int padding,
-    dim3 grid_, dim3 block_,
+    dim3 grid, dim3 block,
     double fx, double fy, double cx, double cy,
     double r1, double r2, double r3,
     double r4, double r5, double r6,
     double r7, double r8, double r9,
     double t1, double t2, double t3)
 {
-    dim3 grid, block;
-    block.x = 32;
-    block.y = 32;
-    grid.x = (width + block.x - 1) / block.x;
-    grid.y = (480 + block.y - 1) / block.y;
-
     project_kernel<<<grid, block>>>(depth, pc, width, padding,
         fx, fy, cx, cy,
         r1, r2, r3,
         r4, r5, r6,
         r7, r8, r9,
         t1, t2, t3);
-
-    //std::cout << "project_kernel: " << cudaGetErrorString(cudaGetLastError()) << std::endl;
 }
 
 __global__ void project_kernel(ushort* depth, double* pc, int width, int padding,
